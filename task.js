c71c13a8ae3663add69eb7f97456ccff16b6ae4d
Ярцев О.Ю <132137309+Drukast@users.noreply.github.com>
2023-12-08 20:53:02 +0700

    add LoadImages function

--- task.js
+++ task.js
@@ -1,3 +1,13 @@
+function LoadImages() {
+    let images = [];
+    for (let i = 0; i < 16; i++) {
+        let img = new Image();
+        img.src = `images/${i + 1}.png`;
+        images.push(img);
+    }
+    return images;
+}
+
 function shuffle(matrix) {  //Функция перемешивает массив matrix в случайном порядке значениями из массива numbers
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
     for (let i = 0; i < 4; i++) {
